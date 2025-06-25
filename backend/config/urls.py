d39ca290b521a7f7d8da1d5f2dfb2908a65b9fff
zx <tanzhuoxuan@gmail.com>
2025-06-25 08:32:01 +0800

    tt

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 """
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+
+def root_view(request):
+    return HttpResponse("Backend API is running smoothly!")
+
 
 from django.contrib import admin
 from django.urls import path, include
@@ -33,4 +38,5 @@ urlpatterns = [
     path("social/", include("social.urls")),
     path("social/accounts/", include("accounts.urls")),
     path('promotions/', include('promotions.urls')),
+    path('/', root_view),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
